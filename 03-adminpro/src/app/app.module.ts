2c62a006f236276f65bb7faf35f4dd97b2cfd230
William Condori Quispe <williamccodori@outlook.com>
2022-06-07 23:56:22 -0500

    add components

--- 03-adminpro/src/app/app.module.ts
+++ 03-adminpro/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotpagefoundComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
